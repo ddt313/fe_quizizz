03702d59107df4e099d673bb90f81600a75c5a36
Thanh Dang <thanh.dang@sioux.asia>
2021-07-18 16:19:09 +0700

    Add assets folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import {Theme} from './theme';
 import reportWebVitals from './reportWebVitals';
+import Routes from './routes';
+
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
+import '@blueprintjs/select';
+import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <Routes />
+    </Theme>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
