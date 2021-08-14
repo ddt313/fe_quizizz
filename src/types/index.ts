0dd3ee2a26611529d13eda58c449d8cee4ced9fb
Thanh Dang <thanh.dang@sioux.asia>
2021-08-14 23:02:31 +0700

    Create type for  store

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,16 @@ export type Pagination = {
   limit: number;
   pageTotal: number;
 };
+
+export type QuestionTable = {
+  _id: string;
+  content: string;
+  level: number;
+  module: string;
+  user: string;
+};
+
+export type SelectBoxType = {
+  _id: string;
+  name: string;
+};
