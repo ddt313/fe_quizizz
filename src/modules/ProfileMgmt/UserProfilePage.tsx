90e80385308a3a97229918c48fe4c6b6b5da3d71
Thanh Dang <thanh.dang@sioux.asia>
2021-08-19 14:36:48 +0700

    Update UI profile

--- src/modules/ProfileMgmt/UserProfilePage.tsx
+++ src/modules/ProfileMgmt/UserProfilePage.tsx
@@ -1,16 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import styled, {css} from 'styled-components';
-import {InputGroup, TextArea, Intent, RadioGroup, Radio, Position, Icon} from '@blueprintjs/core';
+import {InputGroup, RadioGroup, Radio, Position} from '@blueprintjs/core';
 import {DateInput} from '@blueprintjs/datetime';
 import moment from 'moment';
 import {observer} from 'mobx-react';
 
 import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
 import {getMomentFormatter} from '../../infra/Datetime';
-import {countryList, dateFormat, jobGrades} from '../../config';
-import {BaseColor} from '../../theme';
+import {dateFormat} from '../../config';
 import {Role} from '../../types';
 
 import UploadAvatar from './UploadAvatar';
@@ -72,14 +70,14 @@ const UserProfilePage: React.FunctionComponent = observer(() => {
       <Navbar role={Role.lecturer} />
 
       <TitleWrapper>
-        <Title>{'general_information'}</Title>
+        <Title>{'Thông tin Tài khoản'}</Title>
       </TitleWrapper>
 
       <div style={{display: 'flex'}}>
         <Container>
           <ContentWrapper>
             <LabelWrapper>
-              <h2>{'first_name'}</h2>
+              <h2>{'Họ và tên'}</h2>
             </LabelWrapper>
             {inEditMode ? (
               <InputGroup
@@ -102,7 +100,7 @@ const UserProfilePage: React.FunctionComponent = observer(() => {
 
           <ContentWrapper>
             <LabelWrapper>
-              <h2>{'last_name'}</h2>
+              <h2>{'Số điện thoại'}</h2>
             </LabelWrapper>
             {inEditMode ? (
               <InputGroup
@@ -124,7 +122,7 @@ const UserProfilePage: React.FunctionComponent = observer(() => {
           </ContentWrapper>
           <ContentWrapper>
             <LabelWrapper>
-              <h2>{'full_name'}</h2>
+              <h2>{'Địa chỉ thường trú'}</h2>
             </LabelWrapper>
             {inEditMode ? (
               <InputGroup
@@ -139,7 +137,7 @@ const UserProfilePage: React.FunctionComponent = observer(() => {
           </ContentWrapper>
           <ContentWrapper>
             <LabelWrapper>
-              <h2>{'gender'}</h2>
+              <h2>{'Giới tính'}</h2>
             </LabelWrapper>
             {inEditMode ? (
               <RadioGroup
@@ -156,8 +154,8 @@ const UserProfilePage: React.FunctionComponent = observer(() => {
                 selectedValue={genderOptions}
                 inline
               >
-                <Radio label={'male'} value="Male" />
-                <Radio label={'female'} value="Female" />
+                <Radio label={'Male'} value="Male" />
+                <Radio label={'Female'} value="Female" />
               </RadioGroup>
             ) : (
               <RadioGroup
@@ -167,14 +165,14 @@ const UserProfilePage: React.FunctionComponent = observer(() => {
                 // From the blueprintjs documents, RadioGroup must have onChange event.
                 onChange={() => true}
               >
-                <Radio label={'male'} value="Male" />
-                <Radio label={'female'} value="Female" />
+                <Radio label={'Male'} value="Male" />
+                <Radio label={'Female'} value="Female" />
               </RadioGroup>
             )}
           </ContentWrapper>
           <ContentWrapper>
             <LabelWrapper>
-              <h2>{'date_of_birth'}</h2>
+              <h2>{'Ngày sinh'}</h2>
             </LabelWrapper>
             {inEditMode ? (
               <DateInputWrapper error={error && !editingUser.dateOfBirth}>
@@ -197,94 +195,11 @@ const UserProfilePage: React.FunctionComponent = observer(() => {
               <ContentWrapper>{editingUser.dateOfBirth}</ContentWrapper>
             )}
           </ContentWrapper>
-          <ContentWrapper>
-            <LabelWrapper>
-              <h2>{'nationality'}</h2>
-            </LabelWrapper>
-            {inEditMode ? (
-              <SelectWrapper className="bp3-select bp3-fill bp3-large ">
-                <select
-                  onChange={(e) => {
-                    const selectedNationality = countryList.find((data) => data === e.target.value);
-
-                    setEditingUser((oldUser) => ({
-                      ...oldUser,
-                      nationality: selectedNationality || countryList[0],
-                    }));
-                  }}
-                  value={editingUser.nationality}
-                >
-                  <option selected disabled>
-                    {editingUser.nationality}
-                  </option>
-                  {countryList
-                    .filter((data) => data !== editingUser.nationality)
-                    .map((data, index) => (
-                      <option key={index} value={data} label={data} />
-                    ))}
-                </select>
-                <Icon icon="double-caret-vertical" />
-              </SelectWrapper>
-            ) : (
-              <ContentWrapper>{editingUser.nationality}</ContentWrapper>
-            )}
-          </ContentWrapper>
         </Container>
         <Container>
           <ContentWrapper>
             <LabelWrapper>
-              <h2>{'address'}</h2>
-            </LabelWrapper>
-            {inEditMode ? (
-              <TextArea
-                defaultValue={editingUser.address}
-                large={true}
-                fill
-                growVertically
-                style={{resize: 'none'}}
-                intent={error && !editingUser.address ? 'danger' : 'none'}
-                onChange={(e) =>
-                  setEditingUser((oldUser) => ({
-                    ...oldUser,
-                    address: e.target.value,
-                  }))
-                }
-              />
-            ) : (
-              <ContentWrapper>{editingUser.address}</ContentWrapper>
-            )}
-          </ContentWrapper>
-          <ContentWrapper>
-            <LabelWrapper>
-              <h2>{'phone_number'}</h2>
-            </LabelWrapper>
-            {inEditMode ? (
-              <NumberInputGroup
-                defaultValue={editingUser.phoneNumber}
-                maxLength={10}
-                type={'number'}
-                disabled={false}
-                style={{borderRadius: '1rem '}}
-                fill
-                intent={
-                  (error && !editingUser.phoneNumber) || editingUser.phoneNumber.length !== 10
-                    ? 'danger'
-                    : 'none'
-                }
-                onChange={(e) =>
-                  setEditingUser((oldUser) => ({
-                    ...oldUser,
-                    phoneNumber: e.target.value,
-                  }))
-                }
-              />
-            ) : (
-              <ContentWrapper>{editingUser.phoneNumber}</ContentWrapper>
-            )}
-          </ContentWrapper>
-          <ContentWrapper>
-            <LabelWrapper>
-              <h2>{'email'}</h2>
+              <h2>{'Email trường cấp'}</h2>
             </LabelWrapper>
             {inEditMode ? (
               <InputGroup
@@ -309,115 +224,33 @@ const UserProfilePage: React.FunctionComponent = observer(() => {
               <ContentWrapper>{editingUser.email}</ContentWrapper>
             )}
           </ContentWrapper>
-
           <ContentWrapper>
             <LabelWrapper>
-              <h2>{'skype'}</h2>
+              <h2>{'Email Cá nhân'}</h2>
             </LabelWrapper>
             {inEditMode ? (
               <InputGroup
-                defaultValue={editingUser.skype}
+                defaultValue={editingUser.email}
+                type={'email'}
                 disabled={false}
-                style={{borderRadius: '1rem '}}
                 fill
+                style={{borderRadius: '1rem '}}
+                intent={
+                  error && (!editingUser.email || !emailRegex.test(editingUser.email))
+                    ? 'danger'
+                    : 'none'
+                }
                 onChange={(e) =>
                   setEditingUser((oldUser) => ({
                     ...oldUser,
-                    skype: e.target.value,
+                    email: e.target.value,
                   }))
                 }
               />
             ) : (
-              <ContentWrapper>{editingUser.skype}</ContentWrapper>
+              <ContentWrapper>{editingUser.email}</ContentWrapper>
             )}
           </ContentWrapper>
-          <ContentWrapper>
-            <LabelWrapper>
-              <h2>{'position'}</h2>
-
-              {inEditMode ? (
-                <InputGroup
-                  defaultValue={editingUser.position}
-                  disabled={false}
-                  style={{borderRadius: '1rem '}}
-                  fill
-                  intent={error && !editingUser.position ? 'danger' : 'none'}
-                  onChange={(e) =>
-                    setEditingUser((oldUser) => ({
-                      ...oldUser,
-                      position: e.target.value,
-                    }))
-                  }
-                />
-              ) : (
-                <ContentWrapper>{editingUser.position}</ContentWrapper>
-              )}
-            </LabelWrapper>
-          </ContentWrapper>
-          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-            <ContentWrapper>
-              <LabelWrapper>
-                <h2>{'start_working_day'}</h2>
-                {inEditMode ? (
-                  <DateInputWrapper error={error && !editingUser.employmentDate}>
-                    <DateInput
-                      {...getMomentFormatter(dateFormat)}
-                      defaultValue={moment(editingUser.employmentDate, dateFormat).toDate()}
-                      locale="de"
-                      popoverProps={{position: 'auto'}}
-                      fill
-                      inputProps={{leftIcon: 'calendar'}}
-                      onChange={(date: Date) =>
-                        setEditingUser((oldUser) => ({
-                          ...oldUser,
-                          employmentDate: date
-                            ? date.toLocaleDateString('en-GB')
-                            : oldUser.employmentDate,
-                        }))
-                      }
-                    />
-                  </DateInputWrapper>
-                ) : (
-                  <ContentWrapper>{editingUser.employmentDate}</ContentWrapper>
-                )}
-              </LabelWrapper>
-            </ContentWrapper>
-            <ContentWrapper style={{width: '20%'}}>
-              <LabelWrapper>
-                <h2>{'grade'}</h2>
-              </LabelWrapper>
-
-              {inEditMode ? (
-                <SelectWrapper className="bp3-select bp3-fill bp3-large ">
-                  <select
-                    onChange={(e) => {
-                      const selectedGrade = jobGrades.find(
-                        (data) => data === parseInt(e.target.value, 10),
-                      );
-
-                      setEditingUser((oldUser) => ({
-                        ...oldUser,
-                        grade: selectedGrade || jobGrades[0],
-                      }));
-                    }}
-                    value={editingUser.grade}
-                  >
-                    <option selected disabled>
-                      {editingUser.grade}
-                    </option>
-                    {jobGrades
-                      .filter((grade) => grade !== editingUser.grade)
-                      .map((grade, index) => (
-                        <option key={index} value={grade} label={grade.toString()} />
-                      ))}
-                  </select>
-                  <Icon icon="double-caret-vertical" />
-                </SelectWrapper>
-              ) : (
-                <ContentWrapper>{editingUser.grade}</ContentWrapper>
-              )}
-            </ContentWrapper>
-          </div>
         </Container>
         <Container>
           <ContentWrapper>
@@ -427,44 +260,19 @@ const UserProfilePage: React.FunctionComponent = observer(() => {
             {/* Todo: Waiting for API Upload file*/}
             <UploadAvatar inEditMode={inEditMode} />
           </ContentWrapper>
-          <ContentWrapper>
-            <LabelWrapper>
-              <h2>{'About Yourself'}</h2>
-            </LabelWrapper>
-            {inEditMode ? (
-              <TextArea
-                defaultValue={editingUser.description}
-                large={true}
-                intent={Intent.PRIMARY}
-                fill
-                growVertically
-                style={{resize: 'none'}}
-                onChange={(e) =>
-                  setEditingUser((oldUser) => ({
-                    ...oldUser,
-                    description: e.target.value,
-                  }))
-                }
-              />
-            ) : (
-              <ContentWrapper>{editingUser.description}</ContentWrapper>
-            )}
-          </ContentWrapper>
         </Container>
       </div>
       {error && inEditMode && <ErrorMessage>{`error_message.update_users.${error}`}</ErrorMessage>}
       {inEditMode ? (
         <ButtonWrapper>
-          <CancelButton onClick={() => onCancel()}>{'button.cancel'}</CancelButton>
-          <SubmitButton onClick={() => onSubmit()}>{'button.submit'}</SubmitButton>
+          <CancelButton onClick={() => onCancel()}>{'Cancel'}</CancelButton>
+          <SubmitButton onClick={() => onSubmit()}>{'Submit'}</SubmitButton>
         </ButtonWrapper>
       ) : (
         <ButtonWrapper>
-          <SubmitButton onClick={() => setInEditMode(true)}>{'button.edit'}</SubmitButton>
+          <SubmitButton onClick={() => setInEditMode(true)}>{'Edit'}</SubmitButton>
         </ButtonWrapper>
       )}
-
-      <Footer />
     </div>
   );
 });
@@ -480,16 +288,7 @@ const ErrorMessage = styled.div`
     margin-top: 4rem;
   `}
 `;
-const NumberInputGroup = styled(InputGroup)`
-  input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-  input[type='number'] {
-    -moz-appearance: textfield;
-  }
-`;
+
 const TitleWrapper = styled.div`
   height: 100%;
   display: flex;
@@ -574,24 +373,4 @@ const ButtonWrapper = styled.div`
   margin-bottom: 8rem;
   display: flex;
   justify-content: center;
-`;
-
-type SelectProp = {
-  error?: boolean;
-};
-const SelectWrapper = styled.div<SelectProp>`
-  ${({theme, error}) => css`
-    &:after {
-      content: none;
-    }
-    select {
-      background-color: ${BaseColor.white};
-      option:nth-child(1) {
-        color: ${theme.colors.primary};
-      }
-    }
-    border: ${error && 'solid'};
-    border-color: ${error && theme.colors.error};
-    border-width: ${error && '1px'};
-  `}
 `;
